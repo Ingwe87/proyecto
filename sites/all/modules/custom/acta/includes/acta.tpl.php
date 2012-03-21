ca6e0dbc0225159cfe1b47227935327e54771d9a
Javier BM <chichinator9000@gmail.com>
2012-03-21 22:18:42 +0100

    proyecto commit

---
+++ sites/all/modules/custom/acta/includes/acta.tpl.php
@@ -0,0 +1,14 @@
+<div id="node-content" class="acta">
+
+  <div id="left">
+  <?php print $listaparticipantes;?>
+  </div>
+  <div id="right">
+  
+  </div>
+  
+  <div id="bottom">
+  
+  </div>
+
+</div>
